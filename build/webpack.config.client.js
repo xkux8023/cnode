b678302d3c04c2de32d73d0a134f289d436a8fd4
xkux8023 <xkux8023@github.com>
2017-12-03 14:32:00 +0800

    工程配置完成

--- build/webpack.config.client.js
+++ build/webpack.config.client.js
@@ -1,46 +1,23 @@
 const path = require('path')
 const webpack = require('webpack')
+const webpackMerge = require('webpack-merge')
+const baseConfig = require('./webpack.base')
 const HTMLPlugin = require('html-webpack-plugin')
 const isDev = process.env.NODE_ENV === 'development'
 
-const config = {
+const config = webpackMerge(baseConfig, {
   entry: {
     app: path.join(__dirname, '../client/app.js')
   },
   output: {
     filename: '[name].[hash].js',
-    path: path.join(__dirname, '../dist'),
-    publicPath: '/public/'
-  },
-  module: {
-    rules: [
-      {
-        enforce: 'pre',
-        test: /.(js|jsx)$/,
-        loader: "eslint-loader",
-        exclude: [
-          path.resolve(__dirname, '../node_modules')
-        ]
-      },
-      {
-        test: /.jsx$/,
-        loader: 'babel-loader'
-      },
-      {
-        test: /.js$/,
-        loader: 'babel-loader',
-        exclude: [
-          path.join(__dirname, '../node_modules')
-        ]
-      }
-    ]
   },
   plugins: [
     new HTMLPlugin({
       template: path.join(__dirname, '../client/templete.html')
     })
-  ]
-}
+  ],
+})
 
 if (isDev){
   config.entry = {
