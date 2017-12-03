f6b5c8eccc7597a35eb112a48d96af92cd0bcd7d
xkux8023 <xkux8023@github.com>
2017-12-03 14:02:20 +0800

    first commit

---
+++ client/app.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { AppContainer } from 'react-hot-loader' //eslint-disable-line
+import App from './App.jsx' //eslint-disable-line
+
+const root = document.getElementById('root')
+const render = (Component) => {
+  ReactDOM.hydrate(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    root,
+  )
+}
+render(App)
+if (module.hot) {
+  module.hot.accept('./App.jsx', () => {
+    const NextApp = require('./App.jsx').default //eslint-disable-line
+    render(NextApp)
+  })
+}
---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const ReactSSR = require('react-dom/server')
+const fs = require('fs')
+const path = require('path')
+
+
+const isDev = process.env.NODE_ENV === 'development'
+
+
+const app = express()
+
+
+if(!isDev) {
+    const serverEntry = require('../dist/server-entry').default
+    const templete = fs.readFileSync(path.join(__dirname, '../dist/index.html'), 'utf8')
+    app.use('/public', express.static(path.join(__dirname, '../dist')))
+    app.get('*', function (req, res) {
+        const appString = ReactSSR.renderToString(serverEntry)
+        res.send(templete.replace('<!-- app -->', appString))
+    })
+} else {
+    const devStatic = require('./util/dev-static')
+    devStatic(app)
+}
+
+app.listen(3333,function(){
+    console.log('server is listen on port 3333')
+})
